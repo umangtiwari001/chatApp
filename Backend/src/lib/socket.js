afc64149295a1a750720caf09d35eeffb0ceb7f1
umangtiwari001 <umangtiwari088@gmail.com>
2025-09-29 22:27:47 +0530

    Add all deployed frontend URLs to allowedOrigins for CORS

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -8,7 +8,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173",
-       "https://my-chatapp-kjvr.onrender.com"
+       "https://chatapp-6-ktwr.onrender.com",
+      "https://my-chatapp-kjvr.onrender.com"
     ],
     //
       credentials: true
